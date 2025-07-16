fa1caf33324b3c17b987b03eb72f5a5f4763c1a0
ochwada <nakhulolinda@yahoo.com>
2025-07-16 11:46:25 +0200

    initial project setup

---
+++ src/main/java/com/ochwada/dockerstringbootdemo/DockerStringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ochwada.dockerstringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerStringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerStringbootDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ochwada/dockerstringbootdemo/DockerStringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ochwada.dockerstringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerStringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
